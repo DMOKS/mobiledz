cb04a406e8845ef120f0b7e31f8c6a63bcaaad01
DMOKS <aksanka89@rambler.ru>
2022-09-02 01:49:13 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу которая выводит случайное число из отрезка от 10 до 99 и показывает наибольшую цифру числа
+
+Random random = new Random();
+int randomNumber = random.Next(10, 99);
+
+Console.WriteLine(randomNumber);
+
+int firstDigit = randomNumber % 10;
+int secondDigit = randomNumber / 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine(firstDigit);
+}
+else
+{
+    Console.WriteLine(secondDigit);
+}
